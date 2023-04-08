047cd13d3f86db601d308ab10b227b01a5d3fa69
IWojtowicz <73827972+Lizeyy@users.noreply.github.com>
2023-04-08 22:39:34 +0200

    messages

--- src/main/java/com/dconnect/discovery/DiscoveryServerApplication.java
+++ src/main/java/com/dconnect/discovery/DiscoveryServerApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
 @EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
@@ -12,5 +11,4 @@ public class DiscoveryServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
     }
-
 }
